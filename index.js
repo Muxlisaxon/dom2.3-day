413d7e96585d996846f148298ef7628e058213e5
Muxlisaxon <qochqorovamuxlisam@gmail.com>
2023-06-12 11:54:24 +0500

    change bacground

---
+++ index.js
@@ -0,0 +1,48 @@
+// const imgs = document.getElementById('imgs')
+// const prev = document.getElementById('prev')
+// const next = document.getElementById('next')
+
+// const img = document.querySelectorAll('#imgs img');
+
+
+
+
+// let num = 0;
+
+// function myfun() {
+//     if (num > img.length - 1) {
+//         num = 0;
+//     } else if (num < 0) {
+//         num = img.length - 1
+//     }
+//     imgs.style.transform = `translateX(${-num * 1540}px)`;
+// }
+
+
+// prev.addEventListener('click', () => {
+//     num++;
+//     myfun() = `
+// <img src="./img/Hefa2954891de4ba4b99b6413aa6c1cc7L.webp" alt="">
+//     `
+// })
+
+// next.addEventListener('click', () => {
+//     num--;
+//     myfun()
+// })
+
+
+
+
+
+
+
+function prev() {
+    document.getElementById("bd").style.backgroundImage="url(img/1646723089_20-kartinkin-net-p-pluton-kartinki-24.jpg)"
+
+}
+
+
+function next() {
+    document.getElementById("bd").style.backgroundImage="url(img/kolon_mars.png)"
+}
